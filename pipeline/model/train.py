27c3fc4f8922beee52cef41d689af01b24d6ea1c
noah <noah.chasek-macfoy@ibm.com@noahs-MacBook-Pro.local>
2020-10-22 11:19:15 -0600

    implement WOS quality monitoring
    integrated into existing pipeline builder archetecture
    
    used a different data set for binary classification
    used the data.py file to set dataset characteristics
    used made the model available as a variable in model.py rather than a func
    split the data and trained on one set and predict and monitor on another set
    included subscription teardown in pipeline._init_cleanup()
    included wos client initiation in pipeline.set_connection()
    added comments and explanatory doc strings
    ... other less structural changes

--- pipeline/model/train.py
+++ pipeline/model/train.py
@@ -1,28 +1,35 @@
-def train():
+from pipeline.model.data import filename, FEATURE_COLUMNS, LABEL_COLUMN
+import pandas as pd
+from sklearn import svm
 
+train_data = pd.read_csv('train_'+filename)
+val_data = pd.read_csv('val_'+filename)
 
-    from sklearn import datasets
-    iris = datasets.load_iris()
-    digits = datasets.load_digits()
-
-    from sklearn import svm
+def train(data):
     clf = svm.SVC(gamma=0.001, C=100., probability=True)
-    clf.fit(digits.data[:-1], digits.target[:-1])
+    clf.fit(data[FEATURE_COLUMNS], data[LABEL_COLUMN])
     return clf
 
 
 def pickle(clf):
     import pickle
-    s = pickle.dump(clf, open( "model.pickle", "wb" ))
+    MODEL_FILENAME = "model.pickle"
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_path=os.path.join(dir_path, MODEL_FILENAME)
+    with open(file_path, "wb") as f:
+        pickle.dump(clf, f)
 
-    return s
 
-clf = train()
 
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
 
+
+clf = train(train_data)
 pipeline = Pipeline([('svc', clf)])
+pickle(pipeline)
 
-s = pickle(pipeline)
-
+# I know I am predicting on the train set here....
+preds = pipeline.predict(val_data[FEATURE_COLUMNS])
+print(classification_report(val_data[LABEL_COLUMN], preds))
 
