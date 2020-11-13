24170b714581beb5769836fd06466137498f49e0
noah <noah.chasek-macfoy@ibm.com@noahs-MacBook-Pro.local>
2020-11-12 22:27:08 -0700

    update app image build code
    and update jenkins file with new custom built app image
    and clean app dockerfile

--- python/app/build_app_image.sh
+++ python/app/build_app_image.sh
@@ -9,16 +9,25 @@
 # see for using  ssh credentials instead of personal access token:
 # https://docs.openshift.com/container-platform/4.6/builds/creating-build-inputs.html#builds-source-secret-basic-auth_creating-build-inputs
 
+# Switch to `oc project openshift`, the build pull/push secret is correctly set there.
+
 
 export SECRET_NAME=noah-github 
 
+# create secret to clone context dir to build from
 oc create secret generic $SECRET_NAME \
     --from-literal=username=$USERNAME \
     --from-literal=password=$PASSWORD \
     --type=kubernetes.io/basic-auth
 
 export REPO_BRANCH=https://github.ibm.com/Patrick-Harned/mlopspipeline#yaml_refactor
+export BC_NAME=lowes-python-app
 
-oc new-build $REPO_BRANCH --context-dir=python/app --source-secret=$SECRET_NAME
+# create buildConfig and build and push image based on it. 
+oc new-build $REPO_BRANCH --context-dir=python/app --source-secret=$SECRET_NAME --name=$BC_NAME
 
+# only necessary if build from new-build failed.
+oc start-build $BC_NAME
 
+# resulting image
+#image-registry.openshift-image-registry.svc:5000/openshift/lowes-python-app:latest
