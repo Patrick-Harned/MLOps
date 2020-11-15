19da97c8bd2d83bb5ba3033619d24f9e4b461543
noah <noah.chasek-macfoy@ibm.com@noahs-MacBook-Pro.local>
2020-11-14 22:40:16 -0700

    make app expect paths relative to project root
    particularly the passed config.yaml path and the path of the model in the config.yaml
    and when I say project root I mean git repo root

--- python/app/main.py
+++ python/app/main.py
@@ -3,6 +3,8 @@
 if __name__ == "__main__":
     # %load_ext autoreload
     # %autoreload 2
+    #!PROJ_ROOT=../../
+    #!cd $PROJ_ROOT
     
     #import sys, os
     # dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -17,12 +19,12 @@ if __name__ == "__main__":
 
     from pipeline import Pipeline
 
-    def abs_path(root_file, path):
-        '''get abs path of file relative to parent of a reference file'''
-        if os.path.isabs(path):
-            return path
-        _dir = os.path.dirname(os.path.abspath(root_file))
-        return os.path.normpath(os.path.join(_dir, path))
+    # def abs_path(root_file, path):
+    #     '''get abs path of file relative to parent of a reference file'''
+    #     if os.path.isabs(path):
+    #         return path
+    #     _dir = os.path.dirname(os.path.abspath(root_file))
+    #     return os.path.normpath(os.path.join(_dir, path))
 
 
     if len(sys.argv)>1:
@@ -31,13 +33,13 @@ if __name__ == "__main__":
         CONFIG_PATH = '../config.yaml'
 
     # assumes relative paths are all relative to parent dir of main.py!!!
-    CONFIG_PATH = abs_path(__file__, CONFIG_PATH)
+    # CONFIG_PATH = abs_path(__file__, CONFIG_PATH)
     try:   
         with open(CONFIG_PATH) as f:
             config = yaml.safe_load(f)
     except FileNotFoundError as e:
         raise type(e)(f'{e.filename} does not exist. Config file required to run pipeline.')
-    config['Model']['model_path'] = abs_path(CONFIG_PATH, config['Model']['model_path'])
+    # config['Model']['model_path'] = abs_path(CONFIG_PATH, config['Model']['model_path'])
 
 
     pipeline = Pipeline(**config['CP4D'], **config['Model'])
